405a25b7f0718b141a917d30cd4be665dc479204
EXPRESS DEVELOPMENT INC. <Millinershelpingtheyouth@gmail.com>
2026-09-11 04:31:27 -0500

    chore: use new scalable brand favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -86,7 +86,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
       { rel: "manifest", href: "/site.webmanifest" },
     ],
     scripts: [
